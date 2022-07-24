87e427b14ae960049dee3cabb59d27466101e09a
Kronosblaster <davepauljoseph@gmail.com>
2022-07-24 16:55:47 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 
 import './App.css';
-import './components/css/card.css';
 import './components/css/cardPanel.css';
-import './components/css/card.css';
-import './components/css/card.css';
-import './components/css/card.css';
+import './components/css/navBar.css';
+import './components/css/textBlock_intro.css';
+import './components/css/textBlock_join.css';
 
 import NavBar from './components/navBar';
 import CardPanel from './components/cardPanel';
+import Text_intro from './components/textBlock_intro';
+import Text_join from './components/textBlock_join';
+import Footer from "./components/footer";
 
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Text_intro/>
+      <Text_join/>
       <CardPanel /> 
+      <Footer/> 
     </div>
   );
 }
