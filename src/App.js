1f7a880d10836e4ca6754dfa3be4a247575c6e6b
Kronosblaster <davepauljoseph@gmail.com>
2022-07-24 16:16:05 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import React from "react";
+
 import './App.css';
+import './components/css/card.css';
+
+import card from './components/card.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />  
-      </header>
+      <card></card>
     </div>
   );
 }
