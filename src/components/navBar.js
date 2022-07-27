1cf983cafdd63bd325ea55ba072c06f99c23de7b
Kronosblaster <davepauljoseph@gmail.com>
2022-07-27 12:53:02 +0530

    logo

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,13 +2,15 @@ import React from "react";
 import './css/navBar.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from "react-bootstrap/Button";
-import "@fontsource/poppins";
-
+import logo from "./elements/EETICommunity.svg";
 function navBar(){
     return(
+      <div className="navbox">
       <Navbar>
+        <div className="logo"><img src={logo}/></div>
         <Button>Sign Up</Button>
-      </Navbar>
+      </Navbar>    
+      </div>
     );
 }
 
