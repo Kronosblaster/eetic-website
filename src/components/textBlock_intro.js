e2224b3861f5761cda701b57a2ef1de48e3625a0
Kronosblaster <davepauljoseph@gmail.com>
2022-07-28 13:05:50 +0530

    intro

--- src/components/textBlock_intro.js
+++ src/components/textBlock_intro.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './css/textBlock_intro.css';
+import Button from "react-bootstrap/Button";
 function textBlock_intro(){
     return(
     <div className="textBlock_intro">
-        <p>Text Intro</p>
+        <div className="text">
+          <p className="p1">One of the biggest jakdjowej akfjoa</p>
+          <p className="p2">EETIC Community</p>
+          <p className="p3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <Button variant="outlined">Read more</Button>
+        </div>
       </div>
     );
 }
 
 export default textBlock_intro;
+
+ 
