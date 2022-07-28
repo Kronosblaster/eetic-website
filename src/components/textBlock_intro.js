29e88c2b20b4db6bb131af7791a9dfd4f0b745ca
Kronosblaster <davepauljoseph@gmail.com>
2022-07-28 21:54:34 +0530

    images

--- src/components/textBlock_intro.js
+++ src/components/textBlock_intro.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './css/textBlock_intro.css';
 import Button from "react-bootstrap/Button";
+import IMAGES from "..";
+import arc from './site_images/arc.svg'
+import left_arrow from './site_images/arrow-left.svg';
+import right_arrow from './site_images/arrow-right.svg';
+
+
+//const [i,setIndex]=useState(0);
+var i=0;
+/*
+function left(){
+  if(i===0)
+    setIndex(IMAGES.length-1);
+  else
+  setIndex(i-1);
+}
+
+function right(){
+  if(i===IMAGES.length-1)
+    setIndex(0);
+  
+  else
+  setIndex(i+1);
+}
+*/
+function left(){
+  console.log("left");
+}
+function right(){
+  console.log("right");
+}
+
 function textBlock_intro(){
     return(
     <div className="textBlock_intro">
@@ -9,6 +40,10 @@ function textBlock_intro(){
           <p className="p2">EETIC Community</p>
           <p className="p3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
           <Button variant="outlined">Read more</Button>
+          <img src={IMAGES[i]} alt="" className="showReel"/>
+          <img src={arc} className="arc" alt=""/>
+          <img src={left_arrow} className="left" onClick={left} alt=""/>
+          <img src={right_arrow} className="right"onClick={right} alt=""/>
         </div>
       </div>
     );
